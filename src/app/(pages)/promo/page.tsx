0382ac8795c47aa75cd6c87ada8062a393753ae2
Gabriel Alves <ga654066@gmail.com>
2025-04-24 17:37:16 -0300

    feat(promo):initial

---
+++ src/app/(pages)/promo/page.tsx
@@ -0,0 +1,130 @@
+export default function Page() {
+  return (
+    <div className="min-h-dvh grid grid-rows-[4rem_1fr] gap-5">
+      <div className="sticky inset-0 bg-red-500 z-500 ">navbar</div>
+      <div className="grid gap-5 px-2.5">
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          fuga distinctio culpa reprehenderit excepturi tenetur tempora! Nisi,
+          excepturi quam. Magnam at culpa aperiam aliquam, veniam dolorum ipsa
+          libero assumenda harum?
+        </div>
+        <div className="bg-gray-600">ok</div>
+      </div>
+    </div>
+  );
+}
